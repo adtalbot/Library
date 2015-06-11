d146651caaf852e344fd0a6ec167b2328e97d5f7
audrie <audrie.talbot@gmail.com>
2015-06-11 11:42:41 -0700

    Passes spec for find method for book class.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,11 +11,13 @@ describe(Book) do
       expect(test_book.id()).to(eq(1))
     end
   end
+
   describe('.all') do
     it('is empty at first') do
       expect(Book.all()).to(eq([]))
     end
   end
+
   describe('#save') do
     it('adds an book to the database') do
       test_book = Book.new({:name => 'Middlesex', :id => nil})
@@ -23,11 +25,22 @@ describe(Book) do
       expect(Book.all()).to(eq([test_book]))
     end
   end
+
   describe('#==') do
     it('recognizes that two instances with the same name and ID are equal') do
       test_book = Book.new({:name => 'Middlesex', :id => nil})
       test_book2 = Book.new({:name => 'Middlesex', :id => nil})
       expect(test_book).to(eq(test_book2))
+    end
+  end
+
+  describe('.find') do
+    it('returns a book by its ID') do
+      test_book = Book.new({:name => 'Middlesex', :id => nil})
+      test_book.save()
+      test_book2 = Book.new({:name => 'Emma', :id => nil})
+      test_book2.save()
+      expect(Book.find(test_book.id())).to(eq(test_book))
     end
   end
 end

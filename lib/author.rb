792c2103dd7458024da3ce4fc71a26d919b2b339
audrie <audrie.talbot@gmail.com>
2015-06-11 09:47:51 -0700

    Passes spec for find method for author class.

--- lib/author.rb
+++ lib/author.rb
@@ -5,6 +5,7 @@ class Author
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
+
   define_singleton_method(:all) do
     returned_authors = DB.exec("SELECT * FROM authors;")
     authors = []
@@ -15,11 +16,19 @@ class Author
     end
     authors
   end
+
   define_method(:save) do
     result = DB.exec("INSERT INTO authors (name) VALUES ('#{@name}') RETURNING id;")
     @id = result.first.fetch('id').to_i()
   end
+
   define_method(:==) do |another_author|
     self.name().==(another_author.name()).&(self.id().==(another_author.id()))
+  end
+  
+  define_singleton_method(:find) do |id|
+    result = DB.exec("SELECT * FROM authors WHERE id = #{id};")
+    name = result.first.fetch('name')
+    Author.new({:name => name, :id => id})
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -11,11 +11,13 @@ describe(Author) do
       expect(test_author).to(be_an_instance_of(Author))
     end
   end
+
   describe('.all') do
     it('is empty at first') do
       expect(Author.all()).to(eq([]))
     end
   end
+
   describe('#save') do
     it('adds an author to the database') do
       test_author = Author.new({:name => 'Jane Austen', :id => nil})
@@ -23,11 +25,22 @@ describe(Author) do
       expect(Author.all()).to(eq([test_author]))
     end
   end
+
   describe('#==') do
     it('recognizes that two instances with the same name and id are equal') do
       test_author = Author.new({:name => 'Jane Austen', :id => nil})
       test_author2 = Author.new({:name => 'Jane Austen', :id => nil})
       expect(test_author).to(eq(test_author2))
+    end
+  end
+
+  describe('.find') do
+    it('returns an author by its ID') do
+      test_author = Author.new({:name => 'Jane Austen', :id => nil})
+      test_author.save()
+      test_author2 = Author.new({:name => 'Charles Dickens', :id => nil})
+      test_author2.save()
+      expect(Author.find(test_author.id())).to(eq(test_author))
     end
   end
 end

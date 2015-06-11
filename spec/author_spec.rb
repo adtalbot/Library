ec6edca867d8f24bacef5434c23bd6f5288d7c71
audrie <audrie.talbot@gmail.com>
2015-06-11 09:09:40 -0700

    Adds initial files.

---
+++ spec/author_spec.rb
@@ -0,0 +1,14 @@
+require('spec_helper')
+
+describe(Author) do
+  describe('initialize') do
+    it('initializes a new instance of author with a name') do
+      test_author = Author.new({:name => 'Jane Austen', :id => nil})
+      expect(test_author).to(be_an_instance_of(Author))
+    end
+    it('is assigned an id by the database') do
+      test_author = Author.new({:name => 'Jane Austen', :id => 1})
+      expect(test_author).to(be_an_instance_of(Author))
+    end
+  end
+end
